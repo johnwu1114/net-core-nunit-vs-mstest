6b6dba90e9ffef3df686163d552a5954cff60724
John Wu <me@johnwu.cc>
2017-07-19 01:27:14 +0800

    Add unit test lifecycle

---
+++ Example.MSTest/Lifecycle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example.MSTest
+{
+    [TestClass]
+    public class Lifecycle : IDisposable
+    {
+        public Lifecycle()
+        {
+            // 2, 7
+            Debug.WriteLine("Constructor");
+        }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            // 1
+            Debug.WriteLine("ClassInitialize");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            // 12
+            Debug.WriteLine("ClassCleanup");
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // 3, 8
+            Debug.WriteLine("TestInitialize");
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // 5, 10
+            Debug.WriteLine("TestCleanup");
+        }
+
+        [TestMethod]
+        public void FirstTest()
+        {
+            // 4 or 9
+            Debug.WriteLine("FirstTest passes!");
+        }
+
+        [TestMethod]
+        public void SecondTest()
+        {
+            // 4 or 9
+            Debug.WriteLine("SecondTest fails!");
+            Assert.Fail();
+        }
+
+        public void Dispose()
+        {
+            // 6, 11
+            Debug.WriteLine("Dispose");
+        }
+    }
+}
---
+++ Example.NUnit/Lifecycle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace Example.NUnit
+{
+    [TestFixture]
+    public class Lifecycle : IDisposable
+    {
+        public Lifecycle()
+        {
+            // 1
+            Debug.WriteLine("Constructor");
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            // 2
+            Debug.WriteLine("OneTimeSetUp");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            // 10
+            Debug.WriteLine("OneTimeTearDown");
+        }
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            // 3, 6
+            Debug.WriteLine("SetUp");
+        }
+
+        [TearDown]
+        public void TestTearDown()
+        {
+            // 5, 8
+            Debug.WriteLine("TearDown");
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            // 4
+            Debug.WriteLine("FirstTest");
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            // 7
+            Debug.WriteLine("SecondTest");
+            Assert.Fail();
+        }
+
+        public void Dispose()
+        {
+            // 9
+            Debug.WriteLine("Dispose");
+        }
+    }
+}
